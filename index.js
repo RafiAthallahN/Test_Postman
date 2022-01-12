79cc05af9edf598593841fb41865b8a97dccd15e
RafiAthallahN <75708409+RafiAthallahN@users.noreply.github.com>
2022-01-13 06:37:23 +0700

    Update index.js

--- index.js
+++ index.js
@@ -14,6 +14,12 @@ app.get('/orang/:nama', (req,res)=>{
     var namaOrang = req.params.nama
     res.end('Menampilkan nama siswa:'+namaOrang)
 })
+/*
+req.params pada endpoint get ini berfungsi untuk 
+menangkap nilai yang dikirimkan melalui url yang mengirimkan nilai secara 
+langsung tanpa key, misal "Localhost:8080/orang/rayz", maka output yang akan ditampilkan pada 
+variabel namaOrang adalah 'rayz'
+*/
  
 app.post('/orang', (req, res)=>{
     var namaOrang = req.params.nama
@@ -33,7 +39,17 @@ app.put('/orang/:id', (req, res) => {
     var alamat = req.body.alamat
     res.end('Seseorang dengan ID'+id+', telah terupdate')
 });
+/**
+ * Pada endpoint post, delete, dan put diatas, penggunaan req.body berfungsi untuk menangkap nilai yang dikirimkan melalui form-html (interface),
+ * dan untuk bisa menggunakan req.body, kita harus memanggil library body-parser. 
+ * body-parser adalah library untuk menangani application/x-www-form-urlencoded (data json yang dikirim melalui form-html/interface).
+ * 
+ * Dan untuk req.params yang ada pada endpoint put dan delete, memiliki fungsi yang sama seperti pada endpoint get
+ */
  
 app.listen(port, () => {            
     console.log(`Server di port ${port}`)
 })
+/*
+Fungsi ini akan ditampilkan pada terminal untuk memberitahukan port mana yang akan digunakan dalam pengujian
+*/
